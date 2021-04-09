1f7b48a5599f4a8fb34bbe28d8e4b124cf40fcf7
Kerimtay Askar <46015854+akerimtay@users.noreply.github.com>
2021-04-09 23:58:07 +0600

    added: load current user

--- app/src/main/java/com/akerimtay/smartwardrobe/AppModule.kt
+++ app/src/main/java/com/akerimtay/smartwardrobe/AppModule.kt
@@ -6,9 +6,7 @@ import com.akerimtay.smartwardrobe.common.di.InjectionModule
 import com.akerimtay.smartwardrobe.common.persistence.AppPreferences
 import com.akerimtay.smartwardrobe.common.persistence.PreferencesContract
 import com.akerimtay.smartwardrobe.content.ItemContentType
-import com.akerimtay.smartwardrobe.feed.FeedViewModel
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -17,6 +15,5 @@ object AppModule : InjectionModule {
         single<PreferencesContract> { AppPreferences(androidContext()) }
         factory { ContentAdapter(ItemContentType.values()) }
         factory { PagedContentAdapter(ItemContentType.values()) }
-        viewModel { FeedViewModel(get(), get()) }
     }
 }
