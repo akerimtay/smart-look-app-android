5c692e19f3dc98f5c9cc9e7eedd4028bbd718e02
Kerimtay Askar <46015854+akerimtay@users.noreply.github.com>
2021-04-13 22:29:59 +0600

    added: collapsing toolbar for feed screen

--- app/src/main/java/com/akerimtay/smartwardrobe/feed/FeedFragment.kt
+++ app/src/main/java/com/akerimtay/smartwardrobe/feed/FeedFragment.kt
@@ -10,23 +10,13 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.akerimtay.smartwardrobe.R
 import com.akerimtay.smartwardrobe.common.base.BaseFragment
 import com.akerimtay.smartwardrobe.common.persistence.PreferencesContract
-import com.akerimtay.smartwardrobe.common.utils.FormatHelper
-import com.akerimtay.smartwardrobe.common.utils.action
-import com.akerimtay.smartwardrobe.common.utils.getSettingsIntent
-import com.akerimtay.smartwardrobe.common.utils.isNull
-import com.akerimtay.smartwardrobe.common.utils.isPermissionsGranted
-import com.akerimtay.smartwardrobe.common.utils.isPositive
-import com.akerimtay.smartwardrobe.common.utils.observeNotNull
-import com.akerimtay.smartwardrobe.common.utils.shouldShowRequestPermissionsRationale
-import com.akerimtay.smartwardrobe.common.utils.showToast
-import com.akerimtay.smartwardrobe.common.utils.snack
+import com.akerimtay.smartwardrobe.common.utils.*
 import com.akerimtay.smartwardrobe.databinding.FragmentFeedBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import java.util.Date
-import java.util.Locale
-import kotlin.math.roundToInt
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
+import kotlin.math.roundToInt
 
 private val LOCATION_PERMISSIONS = arrayOf(
     Manifest.permission.ACCESS_FINE_LOCATION,
@@ -40,24 +30,9 @@ class FeedFragment : BaseFragment(R.layout.fragment_feed) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            swipeRefreshLayout.setColorSchemeResources(
-                android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light
-            )
-            swipeRefreshLayout.setOnRefreshListener {
-                viewModel.loadWeather(
-                    longitude = 71.4459800f,
-                    latitude = 51.1801000f,
-                )
-            }
-        }
         viewModel.weather.observe(viewLifecycleOwner) { weather ->
-            binding.weatherGroup.isVisible = weather.isNull().not()
+            binding.weatherView.isVisible = weather.isNull().not()
             if (weather != null) {
-                binding.currentDayTextView.text = FormatHelper.getDayMonth(Date())
                 binding.cityNameTextView.text =
                     getString(R.string.city_name_format, weather.cityName, weather.countryCode.orEmpty())
                 binding.iconImageView.setImageResource(weather.iconResId)
