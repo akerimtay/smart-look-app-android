01142ea0f54aa3b6fb609ef24cafb276dd9d377d
Kerimtay Askar <46015854+akerimtay@users.noreply.github.com>
2021-05-24 00:22:37 +0600

    deleted: view pager from feed screen

--- app/src/main/java/com/akerimtay/smartwardrobe/feed/FeedModule.kt
+++ app/src/main/java/com/akerimtay/smartwardrobe/feed/FeedModule.kt
@@ -1,16 +1,13 @@
 package com.akerimtay.smartwardrobe.feed
 
 import com.akerimtay.smartwardrobe.common.di.InjectionModule
-import com.akerimtay.smartwardrobe.feed.ui.list.FeedListViewModel
-import com.akerimtay.smartwardrobe.feed.ui.main.FeedViewModel
-import com.akerimtay.smartwardrobe.user.model.Gender
+import com.akerimtay.smartwardrobe.feed.ui.FeedViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 object FeedModule : InjectionModule {
     override fun create(): Module = module {
-        viewModel { FeedViewModel(get(), get()) }
-        viewModel { (gender: Gender) -> FeedListViewModel(gender, get()) }
+        viewModel { FeedViewModel(get(), get(), get(), get()) }
     }
 }
--- app/src/main/java/com/akerimtay/smartwardrobe/feed/ui/FeedFragment.kt
+++ app/src/main/java/com/akerimtay/smartwardrobe/feed/ui/FeedFragment.kt
@@ -1,4 +1,4 @@
-package com.akerimtay.smartwardrobe.feed.ui.main
+package com.akerimtay.smartwardrobe.feed.ui
 
 import android.Manifest
 import android.content.DialogInterface
@@ -6,28 +6,44 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.DefaultItemAnimator
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.akerimtay.smartwardrobe.R
 import com.akerimtay.smartwardrobe.common.base.BaseFragment
+import com.akerimtay.smartwardrobe.common.base.adapter.PagedContentAdapter
+import com.akerimtay.smartwardrobe.common.di.GlideApp
 import com.akerimtay.smartwardrobe.common.persistence.PreferencesContract
-import com.akerimtay.smartwardrobe.common.ui.ViewPagerAdapter
-import com.akerimtay.smartwardrobe.common.utils.*
+import com.akerimtay.smartwardrobe.common.ui.DefaultItemDecorator
+import com.akerimtay.smartwardrobe.common.utils.action
+import com.akerimtay.smartwardrobe.common.utils.dip
+import com.akerimtay.smartwardrobe.common.utils.getSettingsIntent
+import com.akerimtay.smartwardrobe.common.utils.isNull
+import com.akerimtay.smartwardrobe.common.utils.isPermissionsGranted
+import com.akerimtay.smartwardrobe.common.utils.isPositive
+import com.akerimtay.smartwardrobe.common.utils.observeNotNull
+import com.akerimtay.smartwardrobe.common.utils.shouldShowRequestPermissionsRationale
+import com.akerimtay.smartwardrobe.common.utils.showToast
+import com.akerimtay.smartwardrobe.common.utils.snack
+import com.akerimtay.smartwardrobe.content.ItemContentType
+import com.akerimtay.smartwardrobe.content.LoadStateAdapter
 import com.akerimtay.smartwardrobe.databinding.FragmentFeedBinding
-import com.akerimtay.smartwardrobe.feed.ui.list.FeedListFragment
-import com.akerimtay.smartwardrobe.user.model.Gender
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.tabs.TabLayoutMediator
 import java.util.*
 import kotlin.math.roundToInt
+import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 private val LOCATION_PERMISSIONS = arrayOf(
     Manifest.permission.ACCESS_FINE_LOCATION,
     Manifest.permission.ACCESS_COARSE_LOCATION
 )
 
-private val GENDERS_FOR_HEADER = listOf(Gender.MALE, Gender.FEMALE)
+private const val DIVIDER_SIZE = 16
+private const val VERTICAL_SIZE = 8
+private const val HORIZONTAL_SIZE = 4
 
 class FeedFragment : BaseFragment(R.layout.fragment_feed) {
     private val binding: FragmentFeedBinding by viewBinding()
@@ -36,32 +52,56 @@ class FeedFragment : BaseFragment(R.layout.fragment_feed) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            val viewPagerAdapter = ViewPagerAdapter(this@FeedFragment)
-            viewPagerAdapter.addFragments(
-                listOf(
-                    FeedListFragment.create(GENDERS_FOR_HEADER[0]),
-                    FeedListFragment.create(GENDERS_FOR_HEADER[1])
+        val glide = GlideApp.with(this)
+        val contentAdapter = get<PagedContentAdapter<ItemContentType>> { parametersOf(glide) }
+        with(contentAdapter) {
+            addLoadStateListener { loadState ->
+                binding.emptyStateView.isVisible = loadState.source.refresh is LoadState.NotLoading &&
+                        loadState.append.endOfPaginationReached &&
+                        itemCount < 1
+                binding.swipeRefreshLayout.isRefreshing = loadState.refresh is LoadState.Loading
+            }
+            withLoadStateHeaderAndFooter(
+                header = LoadStateAdapter(this),
+                footer = LoadStateAdapter(this)
+            )
+        }
+        with(binding.recyclerView) {
+            adapter = contentAdapter
+            itemAnimator = DefaultItemAnimator().apply { supportsChangeAnimations = false }
+            setHasFixedSize(true)
+            addItemDecoration(
+                DefaultItemDecorator(
+                    right = dip(HORIZONTAL_SIZE),
+                    left = dip(HORIZONTAL_SIZE),
+                    bottom = dip(VERTICAL_SIZE),
+                    divider = dip(DIVIDER_SIZE),
                 )
             )
-            viewPager.apply { adapter = viewPagerAdapter }
-            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-                val item = GENDERS_FOR_HEADER[position]
-                tab.text = when (item) {
-                    Gender.MALE -> getString(item.displayName)
-                    Gender.FEMALE -> getString(item.displayName)
-                }
-            }.attach()
+        }
+        with(binding.swipeRefreshLayout) {
+            setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light
+            )
+            setOnRefreshListener { contentAdapter.refresh() }
         }
 
         viewModel.weather.observe(viewLifecycleOwner) { weather ->
             binding.weatherView.isVisible = weather.isNull().not()
             if (weather != null) {
-                binding.cityNameTextView.text =
-                    getString(R.string.city_name_format, weather.cityName, weather.countryCode.orEmpty())
+                binding.cityNameTextView.text = getString(
+                    R.string.city_name_format,
+                    weather.cityName,
+                    weather.countryCode.orEmpty()
+                )
                 binding.iconImageView.setImageResource(weather.iconResId)
                 binding.temperatureTextView.text = formatTemperature(weather.temperature)
-                binding.descriptionTextView.text = weather.description.capitalize(Locale.ROOT)
+                binding.descriptionTextView.text = weather.description.replaceFirstChar {
+                    if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString()
+                }
                 binding.feelsLikeValueTextView.text = formatTemperature(weather.feelsLike)
             }
         }
@@ -69,6 +109,10 @@ class FeedFragment : BaseFragment(R.layout.fragment_feed) {
             when (action) {
                 is FeedAction.ShowMessage -> showToast(messageResId = action.messageResId)
             }
+        }
+        viewModel.outfits.observeNotNull(viewLifecycleOwner) { pagingData ->
+            contentAdapter.submitData(viewLifecycleOwner.lifecycle, pagingData)
+            binding.emptyStateView.isVisible = false
         }
 
         when {
